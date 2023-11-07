6a9e828960f250f1fccbb5852e6988d20e3a55a2
justgithubaccount <kulikovyevgeny@outlook.com>
2023-11-07 20:57:44 +0500

    v1.1

--- index.js
+++ index.js
@@ -3,36 +3,18 @@ require('dotenv').config();
 const tgToken = process.env.TELEGRAM_TOKEN;
 
 const TelegramBot = require('node-telegram-bot-api');
+const {gameSet, gameAgain} = require('./options.js')
 
 const options = {
     polling: true
 };
-
-const gameSet = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{ text: '1', callback_data: '1' }, { text: '2', callback_data: '2' }, { text: '3', callback_data: '3' }],
-            [{ text: '4', callback_data: '4' }, { text: '5', callback_data: '5' }, { text: '6', callback_data: '6' }],
-            [{ text: '7', callback_data: '7' }, { text: '8', callback_data: '8' }, { text: '9', callback_data: '9' }],
-            [{ text: '0', callback_data: '0' }],
-        ]
-    })
-}
-
-const gameAgain = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{ text: 'Попробовать еще раз', callback_data: '/again' }],
-        ]
-    })
-}
 
 const dataChats = {};
 
 const startGame = async (chatId) => {
     const randomNumber = String(Math.floor(Math.random() * 10));
     dataChats[chatId] = randomNumber;
-    
+
     await bot.sendMessage(chatId, `Тебе нужно будет угадать число от 1 до 9`);
     await bot.sendMessage(chatId, `Начинай отгадывать число`, gameSet);
 }
